82933a4c1d49ab23e8af4c5d9c3ecf4aa6d881cd
Anderson Vargas <vs71374902@gmail.com>
2026-07-01 23:09:38 -0400

    feat: implementar módulo de ventas

--- src/app.js
+++ src/app.js
@@ -2,4 +2,14 @@ function iniciarSistema() {
     console.log("Comercializadora Santa Cruz S.R.L.");
 }
 
+function gestionarInventario() {
+    console.log("Módulo de inventario cargado.");
+}
+
+function gestionarVentas() {
+    console.log("Módulo de ventas cargado.");
+}
+
 iniciarSistema();
+gestionarInventario();
+gestionarVentas();
