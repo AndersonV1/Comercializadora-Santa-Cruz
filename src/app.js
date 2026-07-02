40b00a03bdbb53fb69612be509c64bafb3c64b7d
Anderson Vargas <vs71374902@gmail.com>
2026-07-01 23:08:09 -0400

    feat: implementar módulo de inventario

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ function iniciarSistema() {
     console.log("Comercializadora Santa Cruz S.R.L.");
 }
 
+function gestionarInventario() {
+    console.log("Módulo de inventario cargado.");
+}
+
 iniciarSistema();
+gestionarInventario();
